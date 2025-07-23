b25d9241a773ee1c0318d83844f5e40e170f5c11
AndreiTheG <andrei.alexandru.gheorghe@gmail.com>
2025-07-23 09:50:31 +0300

    Added the post methods of fibonacci, factorial and power and get methods of existent operations of fibonacci, factorial or power from the history saved data

--- routes/routes.py
+++ routes/routes.py
@@ -1,24 +1,73 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import APIRouter, HTTPException, Query
 import math
-from contextlib import asynccontextmanager
-from database.connectionDB import app, get_db_connection, init_db, save_operation
+from database.connectionDB import get_db_connection, save_operation
 from functions.fibonacci import fibonacci
+from model.fibonacci_input_model import FibonacciInput
+from model.factorial_input_model import FactorialInput
+from model.power_input_model import PowerInput
 
-# Set the lifespan of the server app
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    init_db()
-    yield
+# We define the router to call the routes
+router = APIRouter()
+
+
+# Post method for inserting and saving a fibonacci operation in database
+@router.post("/fibonacci")
+def post_fibonacci(data: FibonacciInput):
+    try:
+        result = fibonacci(data.n)
+        save_operation("fibonacci", f"n={data.n}", str(result))
+        return {
+            "operation": "fibonacci",
+            "n": data.n,
+            "result": result
+        }
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
+# Post method for inserting and saving a factorial operation in database
+@router.post("/factorial")
+def post_factorial(data: FactorialInput):
+  try:
+      result = factorial(data.n)
+      save_operation("fibonacci", f"n={data.n}", str(result))
+      return {
+          "operation": "factorial",
+          "n": data.n,
+          "result": result
+      }
+      except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+#Post method for inserting and saving a power operation in database
+@router.post("/pow")
+def post_power(data: PowerInput):
+    try:
+        result = math.pow(data.base, data.exp)
+        save_operation("pow", f"{data.base}^{data.exp}", str(result))
+        return {
+            "operation": "pow",
+            "base": data.base,
+            "exp": data.exp,
+            "result": result
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
 
 # This method will get and display the rows that used the pow operation
-@app.get("/pow")
+@router.get("/pow")
 def compute_power(base: float = Query(...), exp: float = Query(...)):
     result = math.pow(base, exp)
     save_operation("pow", f"{base}^{exp}", str(result))
     return {"operation": "pow", "base": base, "exp": exp, "result": result}
 
+
+
 # This method will get and display a row that used the fibonacci operation
-@app.get("/fibonacci")
+@router.get("/fibonacci")
 def compute_fibonacci(n: int = Query(..., ge=0)):
     try:
         result = fibonacci(n)
@@ -27,8 +76,10 @@ def compute_fibonacci(n: int = Query(..., ge=0)):
     save_operation("fibonacci", f"n={n}", str(result))
     return {"operation": "fibonacci", "n": n, "result": result}
 
+
+
 # This method will get and display a row that used the fibonacci operation
-@app.get("/factorial")
+@router.get("/factorial")
 def compute_factorial(n: int = Query(..., ge=0)):
     try:
         result = math.factorial(n)
@@ -37,12 +88,45 @@ def compute_factorial(n: int = Query(..., ge=0)):
     save_operation("factorial", f"n={n}", str(result))
     return {"operation": "factorial", "n": n, "result": result}
 
+
+
 # This method is used to display an historian of all operations that have been
 # executed and saved
-@app.get("/history")
+@router.get("/history")
 def get_history():
     conn = get_db_connection()
     cursor = conn.cursor()
     rows = cursor.execute("SELECT * FROM operations ORDER BY timestamp DESC").fetchall()
+    conn.close()
+    return [dict(row) for row in rows]
+
+
+@router.get("/history/fibonacci")
+def get_fibonacci_history():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    rows = cursor.execute(
+        "SELECT * FROM operations WHERE operation = 'fibonacci' ORDER BY timestamp DESC"
+    ).fetchall()
+    conn.close()
+    return [dict(row) for row in rows]
+
+@router.get("/history/factorial")
+def get_factorial_history():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    rows = cursor.execute(
+        "SELECT * FROM operations WHERE operation = 'factorial' ORDER BY timestamp DESC"
+    ).fetchall()
+    conn.close()
+    return [dict(row) for row in rows]
+
+@router.get("/history/pow")
+def get_fibonacci_history():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    rows = cursor.execute(
+        "SELECT * FROM operations WHERE operation = 'pow' ORDER BY timestamp DESC"
+    ).fetchall()
     conn.close()
     return [dict(row) for row in rows]
