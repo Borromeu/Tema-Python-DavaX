99c59555ecd7b3bfabbec9a71ca93a60f41dd9e2
AndreiTheG <andrei.alexandru.gheorghe@gmail.com>
2025-07-23 09:54:35 +0300

    Added the main method to start the FastAPI application and to automatically load the routes using include_routes

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from database.connectionDB import init_db
+from routes.routes import router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+# Create the object of FastAPI
+app = FastAPI(
+    title="Math Microservice with SQLite",
+    lifespan=lifespan
+)
+
+# This code line defines the existent routes from routes/routes.py
+app.include_router(router)
